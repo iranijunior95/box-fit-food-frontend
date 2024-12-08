fdc2e2c5cdd4093038d0a64a3c9ca2670c107c4b
irani junior <irani-junior@irani-junior-Inspiron-3442.(none)>
2024-12-08 19:08:39 -0300

    commit: ajuste app

--- src/App.jsx
+++ src/App.jsx
@@ -21,62 +21,7 @@ function App() {
         isOpenMenu={isOpenMenu} 
       />
 
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
-      <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Fugit tenetur rerum consectetur, inventore voluptatem autem! 
-      Eius ratione sapiente expedita odit quas 
-      eligendi at voluptates quibusdam neque, voluptas eos rem autem.
-      </p>
-      <br />
+  
     </>
   )
 }
